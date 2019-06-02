a03e076039a79df5151e9f6f070edf00ba040c2e
rodrigo-sm <rodrigo_sm@outlook.com.br>
2019-06-02 18:56:16 -0300

    Correçao da Ex.3 da revisao de LP-1

--- 3 SEMESTRE/Linguagens-de-Programacao-1/Revisao/3/3-C.py
+++ 3 SEMESTRE/Linguagens-de-Programacao-1/Revisao/3/3-C.py
@@ -7,10 +7,10 @@ class Quadrilatero(object):
         self.__altura = a
 
     def calcular_area(self):
-        return b*a
+        return self.__base*self.__altura
 
     def calcular_perimetro(self):
-        return b+b+a+a
+        return self.__base*2 + 2*self.__altura
 
 class Quadrado(Quadrilatero):
     __lado = 0.0
@@ -31,5 +31,13 @@ class Trapezio(Quadrilatero):
         Quadrilatero.__init__(self, bmenor+bmaior, alt)
 
     def calcular_area(self):
-        return Quadrilatero.calcular_area() / 2
+        return Quadrilatero.calcular_area(self) / 2
 
+thislist = [Quadrilatero(1,2), Quadrado(4), Trapezio(2,3,4)]
+sumArea = 0
+sumPerimetro = 0
+for i in thislist:
+    sumArea = sumArea + i.calcular_area()
+    sumPerimetro = sumPerimetro + i.calcular_perimetro()
+print("Soma da area: {}".format(sumArea))
+print("Soma do perimetro: {}".format(sumPerimetro))
